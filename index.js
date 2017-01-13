bccb6e5af5556ebde2a0fb009cbe52245f704a93
Noah Rosenzweig <noah@rays-lab.com>
2017-01-13 16:36:22 +0100

    initial commit, basic functionality done, needs css styling

---
+++ index.js
@@ -0,0 +1,64 @@
+
+import express from "express";
+import socket from "socket.io";
+import chalk from "chalk";
+import datetime from "node-datetime";
+
+
+const port = 5555;
+const app = express();
+const server = app.listen(port);
+app.use(express.static("public"));
+const io = socket(server);
+
+console.log(chalk.green("server running on port " + port));
+
+let chat = [];
+let connectName = "";
+
+// connection
+io.sockets.on("connection", (socket) => {
+
+	// get ip and id
+	const ip = socket.handshake.address.substr(7);
+	const id = socket.id;
+	// log connection
+	console.log(chalk.green(chalk.underline(curDate("H:M:S")) + " - connected: " + chalk.bold(id + " - " + ip)));
+
+	// add new connection to chat
+	socket.on("newConnection", (name) => {
+		// set name if not set yet
+		if (connectName == "") {
+			connectName = name;
+		}
+		chat.push({ name: "", time: curDate("H:M:S"), text: `<td class="sysMsg"><b>${name}</b> joined the chat.</td>` });
+		// send chat history
+		io.sockets.emit("chat", chat);
+	});
+
+	// new chat message
+	socket.on("newMsg", (data) => {
+		chat.push({ name: data.name, time: curDate("H:M:S"), text: `<td class="userMsg">${data.text}</td>` });
+		// send chat history
+		io.sockets.emit("chat", chat);
+	});
+
+
+
+	// disconnection
+	socket.on("disconnect", () => {
+		// send chat diconnection info
+		chat.push({ name: "", time: curDate("H:M:S"), text: `<td class="sysMsg"><b>${connectName}</b> left the chat.</td>` });
+		// send chat history
+		io.sockets.emit("chat", chat);
+		// log disconnection
+		console.log(chalk.red(chalk.underline(curDate("H:M:S")) + " - disconnected: " + chalk.bold(socket.id + " - " + ip)));
+	});
+
+});
+
+
+
+function curDate(frmt) {  // get date and/or time with format
+	return datetime.create().format(frmt);
+}
---
+++ public/main.js
@@ -0,0 +1,127 @@
+
+const body = document.querySelector("body");
+const loginDiv = document.querySelector(".loginDiv");
+const loginInput = document.querySelector("#loginInput");
+const loginBtn = document.querySelector("#loginBtn");
+const chatbox = document.querySelector(".chatboxDiv");
+
+const port = 5555;
+let socket;
+let id;
+let name;
+// let chatActive = false;
+
+let chat;
+// let input;
+// let btn;
+
+
+// login
+function login() {
+	// console.log("login function called");
+
+	if (loginInput.value.length > 0) {  // name approved
+		name = loginInput.value;  // set name
+		// loginInput.removeAttribute("autofocus");
+		body.removeChild(loginDiv);  // remove login div
+		// create p with login name
+		let p = document.createElement("p");
+		p.id = "loginName";
+		p.innerHTML = `Logged in as <b>${name}</b>`;
+		// body.appendChild(p);
+		body.prepend(p);
+		// enable chatbox
+		// chatActive = true;
+		startChat();
+
+	} else {  // name invalid
+		console.log("not valid");
+		let p = document.createElement("p");
+		p.id = "nameInvalid";
+		p.innerHTML = "Username not valid, please enter new username.";
+		loginDiv.appendChild(p);
+	}
+
+}
+
+
+// chatbox
+// on connection
+function startChat() {
+	// connect to socket
+	socket = io.connect("http://192.168.178.86:" + port);
+
+	// emit new connection
+	socket.emit("newConnection", name);
+
+	// set socket receivers
+	socket.on("chat", updateChat);  // get chat history
+
+	
+
+}
+
+// update chatbox
+function updateChat(data) {
+
+	chat = data;
+
+	let table = document.createElement("table");
+	table.id = "chatTable";
+	let content = "";
+
+	chat.forEach((msg,index) => {
+		content += `
+			<tr>
+			<th class="timestamp"><u>${msg.time}</u></th>
+			<th class="name">${msg.name}</th>
+			${msg.text}
+			</tr>
+		`;
+	});
+
+	table.innerHTML = content;
+	chatbox.innerHTML = "";
+	chatbox.appendChild(table);
+
+	chatbox.scrollTop = chatbox.scrollHeight;
+
+	// console.log(content);
+
+	mkInput();
+
+}
+
+// send msg
+function send(msg) {
+	let data = {
+		name: name,
+		text: msg
+	}
+	socket.emit("newMsg", data);
+}
+
+// create input field and submit button
+function mkInput() {
+	// create input field
+	let input = document.createElement("input");
+	input.className = "msgInput";
+	input.setAttribute("type", "text");
+	// input.setAttribute("autofocus", "");
+	// input.focus();
+	chatbox.appendChild(input);
+	document.querySelector(".msgInput").focus();
+	// create submit button
+	let btn = document.createElement("button");
+	btn.className = "inputBtn";
+	btn.innerHTML = "Send";
+	chatbox.appendChild(btn);
+	// set event listener for button
+	btn.addEventListener("click", () => send(input.value));
+	input.addEventListener("keydown", (event) => {if (event.key === "Enter") send(input.value)});
+}
+
+
+// login event listeners
+loginBtn.addEventListener("click", login);
+loginInput.addEventListener("keydown", (event) => {if (event.key === "Enter") login()});
